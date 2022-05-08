5f30b7d51b03970531b3f36e32085ecea2fd1ee6
Igor <48488039+hjphopchop@users.noreply.github.com>
2022-05-09 02:11:41 +0300

    add LoginForm base

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import cl from "./LoginForm.module.css"
+import React from "react";
+import cl from "./LoginForm.module.css";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  login: string;
+  password: string;
+};
 
 const LoginForm = () => {
-  return (
-    <div className={cl.main}>LoginForm</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => alert("vhod");
 
-export default LoginForm
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={cl.form}>
+      <input className={cl.input} {...register("login", { required: true })} />
+
+      <input
+        type="password"
+        className={cl.input}
+        {...register("password", { required: true })}
+        
+     />
+{errors.password && <span >обязательное поле</span>}
+   
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default LoginForm;
